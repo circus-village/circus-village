dfc276c29a96cbf35eb535e14f7b2a0cf7bf7679
muji <freeformsystems@gmail.com>
2016-05-10 09:57:29 +0800

    Move events task to module.

---
+++ events.js
@@ -0,0 +1,71 @@
+var moment = require('moment')
+  , path = require('path')
+  , fs = require('fs-extra');
+
+// @task events build the events (order by date desc)
+function events(cb) {
+  var source = 'doc/events'
+    , files = fs.readdirSync(source)
+    , list = [];
+
+  function build() {
+    var output = 'doc/events.md'
+      , stream
+      , sorted = list.sort(function(a, b) {
+          a = a.start;
+          b = b.start;
+          if(a === b) {
+            return 0;
+          }
+          return a < b ? 1 : -1;
+        })
+
+    stream = fs.createWriteStream(output);
+
+    stream.once('open', function() {
+      for(var i = 0;i < sorted.length;i++) {
+        stream.write(sorted[i].contents + '\n'); 
+      }
+      stream.end();
+    })
+
+    stream.once('finish', cb);
+  }
+
+  function next(err) {
+    if(err) {
+      return cb(err);
+    } 
+    var file = files.shift();
+    if(!file) {
+      return build();
+    }
+    fs.readFile(path.join(source, file), function(err, contents) {
+      if(err) {
+        return cb(err); 
+      }
+
+      contents = '' + contents;
+      var start
+        , end;
+
+      contents.replace(/data-start="([^"]+)"\s+data-end="([^"]+)"/, 
+        function(all, begin, finish) {
+          start = begin;
+          end = finish;
+        }
+      )
+
+      start = moment(start, 'DD/MM/YYYY');
+      end = moment(end, 'DD/MM/YYYY');
+
+      list.push({file: file, start: start, end: end, contents: contents});
+
+      next();
+    })
+  }
+
+  next();
+}
+
+module.exports = events;
--- mkdoc.js
+++ mkdoc.js
@@ -1,74 +1,10 @@
 var mk = require('mktask')
-  , moment = require('moment')
-  , path = require('path')
+  , events
   , bs
   , fs = require('fs-extra');
 
 // @task events build the events (order by date desc)
-function events(cb) {
-  var source = 'doc/events'
-    , files = fs.readdirSync(source)
-    , list = [];
-
-  function build() {
-    var output = 'doc/events.md'
-      , stream
-      , sorted = list.sort(function(a, b) {
-          a = a.start;
-          b = b.start;
-          if(a === b) {
-            return 0;
-          }
-          return a < b ? 1 : -1;
-        })
-
-    stream = fs.createWriteStream(output);
-
-    stream.once('open', function() {
-      for(var i = 0;i < sorted.length;i++) {
-        stream.write(sorted[i].contents + '\n'); 
-      }
-      stream.end();
-    })
-
-    stream.once('finish', cb);
-  }
-
-  function next(err) {
-    if(err) {
-      return cb(err);
-    } 
-    var file = files.shift();
-    if(!file) {
-      return build();
-    }
-    fs.readFile(path.join(source, file), function(err, contents) {
-      if(err) {
-        return cb(err); 
-      }
-
-      contents = '' + contents;
-      var start
-        , end;
-
-      contents.replace(/data-start="([^"]+)"\s+data-end="([^"]+)"/, 
-        function(all, begin, finish) {
-          start = begin;
-          end = finish;
-        }
-      )
-
-      start = moment(start, 'DD/MM/YYYY');
-      end = moment(end, 'DD/MM/YYYY');
-
-      list.push({file: file, start: start, end: end, contents: contents});
-
-      next();
-    })
-  }
-
-  next();
-}
+events = require('./events');
 
 function sync(/*cb*/) {
   var chokidar = require('chokidar');
@@ -182,7 +118,8 @@ function gallery(cb) {
 
   function done() {
     console.error(list) 
-    fs.writeFileSync('lib/gallery.json', JSON.stringify(list, undefined, 2));
+    fs.writeFileSync(
+      'lib/gallery.json', JSON.stringify(list, undefined, 2));
     cb();
   }
 
@@ -199,7 +136,6 @@ function gallery(cb) {
     }
 
     file = pth + name;
-    //console.error(file);
 
     try {
       new ExifImage({image : file}, function (err, data) {
